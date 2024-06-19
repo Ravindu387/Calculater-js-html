1ff6dfe28d04946bb48cfb1fe349438f01f9b027
Ravindu387 <ravindunim6121@gmail.com>
2024-06-19 10:13:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+function calcadd(){
+    let num1=parseInt(document.getElementById("num 1").value);
+    let num2=parseInt(document.getElementById("num 2").value);
+    let lblOutput=document.getElementById("output1");
+    let answer=num1+num2;
+
+    lblOutput.innerHTML=answer;
+
+}
+
+function calcmin(){
+    let num1=parseInt(document.getElementById("num 3").value);
+    let num2=parseInt(document.getElementById("num 4").value);
+    let lblOutput=document.getElementById("output2");
+    let answer=num1-num2;
+
+    lblOutput.innerHTML=answer;
+
+}
+
+function calcmul(){
+    let num1=parseInt(document.getElementById("num 5").value);
+    let num2=parseInt(document.getElementById("num 6").value);
+    let lblOutput=document.getElementById("output3");
+    let answer=num1*num2;
+
+    lblOutput.innerHTML=answer;
+
+}
+
+function calcdiv(){
+    let num1=parseInt(document.getElementById("num 7").value);
+    let num2=parseInt(document.getElementById("num 8").value);
+    let lblOutput=document.getElementById("output4");
+    let answer=num1/num2;
+
+    lblOutput.innerHTML=answer;
+
+}
+
