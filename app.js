30f0aa7a1e128ee76a6f242d655063345e6516e4
Ravindu387 <ravindunim6121@gmail.com>
2024-06-19 10:59:39 +0530

    4th commit

--- app.js
+++ app.js
@@ -20,46 +20,6 @@ function calc(){
         lblOutput.innerHTML=answer;
 
 
-    //     num1=new Number(document.getElementById("num 1").value);
-    //      num2=new Number(document.getElementById("num 2").value); 
-    //     let lblOutput=document.getElementById("output1");
-    //     let answer=num1+num2;
-    
-    //    lblOutput.innerHTML=answer;
+
  }
 
-//  function calcmin(){
-//     let num1="";
-//     let num2="";
-
-//      num1=new Number(document.getElementById("num 3").value);
-//      num2=new Number(document.getElementById("num 4").value);
-//     let lblOutput=document.getElementById("output2");
-//     let answer=num1-num2;
-
-//    lblOutput.innerHTML=answer;
-// }
-
-// function calcmul(){
-//     let num1="";
-//     let num2="";
-
-//      num1=new Number(document.getElementById("num 5").value);
-//      num2=new Number(document.getElementById("num 6").value);
-//     let lblOutput=document.getElementById("output3");
-//     let answer=num1*num2;
-
-//    lblOutput.innerHTML=answer;
-// }
-
-// function calcdiv(){
-//     let num1="";
-//     let num2="";
-
-//      num1=new Number(document.getElementById("num 7").value);
-//      num2=new Number(document.getElementById("num 8").value);
-//     let lblOutput=document.getElementById("output4");
-//     let answer=num1/num2;
-
-//    lblOutput.innerHTML=answer;
-// }
